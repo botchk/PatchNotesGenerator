9a655cb1331492f8bf517dbb211428cb50663990
yeahservice <dom.augustin@gmx.at>
2017-08-16 20:13:48 +0200

    Added parsing of patch summaries and iteration of patch urls

--- generator.py
+++ generator.py
@@ -10,22 +10,26 @@ def get_texts(url):
 
     text = ''
     request = requests.get(url)
-    soup = BeautifulSoup(request.text, "html.parser")
-    changes = soup.findAll("div", {"class": "patch-change-block"})
-    for change in changes:
-        contexts = change.findAll("blockquote", {"class": "blockquote context"})
-        for context in contexts:
+    
+    if request.status_code == requests.codes.ok:
+        request.raise_for_status()
+        
+        soup = BeautifulSoup(request.text, "html.parser")
+        container = soup.find("div", {"id": "patch-notes-container"})
+        summary = container.find_next("blockquote", {"class": "blockquote context"})
+        #for context in contexts:
             # .text removes all kind of html tags
-            text = text + context.text.replace("\t", " ").replace("\n", " ") + ' '
-
+        text = text + summary.text.replace("\t", " ").replace("\n", " ") + ' '
+    else:
+        print("    ERROR status_code: " + str(request.status_code))
     return text
     
 def main():
     
     # Check that the config file exists
     if not os.path.isfile("config.py"):
-        print("You must create a config file")
-        print("Please see config.example.py")
+        print("config.py not found")
+        print("see config.example.py")
         exit(1)
 
     #account = Account(REDDIT_USERNAME, REDDIT_PASSWORD, "PGdevTest 0.1")
@@ -35,13 +39,22 @@ def main():
     #submission = account.post_url_submission("test", "this is a test", "http://imgur.com/gallery/eydpv2P")
     
     text = ''
-    for i in range(1, 18):
-        text = text + get_texts('http://euw.leagueoflegends.com/en/news/game-updates/patch/patch-6' + str(i) + '-notes')
+    base_url = 'http://euw.leagueoflegends.com/en/news/game-updates/patch/patch-'
+    patches = {5:24, 6:24, 7:16}
+    
+    print("Parsing: ")
+    for year, max_number in patches.items():
+        for number in range(1, max_number + 1):
+            url = base_url + str(year) + str(number) + '-notes'
+            print("  * " + url)
+            text = text + get_texts(url).encode("utf-8")
     
     model = markovify.Text(text, state_size=2)
 
-    for i in range(100):
-      print(model.make_sentence())
+    with open("out.txt", "w") as file:
+        for i in range(100):
+            file.write(str(model.make_sentence()))
+            file.write("\n")
 
 if __name__ == "__main__":
     main()
