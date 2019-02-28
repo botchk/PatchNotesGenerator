7e3c51a043533d1ccf6fd7b45de37537e0216267
yeahservice <dom.augustin@gmx.at>
2019-02-28 23:47:15 +0100

    Changed generator to save a generated patch in json format to a file

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ import markovify
 import codecs
 import json
 
+from patch import Patch
 from patch import Champion
 from patch import serialize as patch_serialize
 
@@ -32,7 +33,7 @@ def generate_summary(summaries):
         while sentence == None or "Welcome" in sentence:
             sentence = corpus_model.make_sentence()
             
-        summary = summary + '\n' + sentence
+        summary = summary + ' ' + sentence
     
     return summary
 
@@ -52,7 +53,7 @@ def generate_sentence(input, attempts, state_size):
         
 def main(): 
     patches = {}
-    champions = {}
+    merged_champions = {}
     
     with codecs.open(os.path.join(data_dir, "patches"), "r", "utf-8") as file:
         patches = json.load(file)
@@ -60,14 +61,26 @@ def main():
     summaries = ""
     for patch in patches.values():
         summaries += patch['summary']
+        for champion in patch['champions']:
+            if champion['name'] in merged_champions:
+                merged_champions[champion['name']].summary += ' ' + champion['summary']
+                merged_champions[champion['name']].description += ' ' + champion['description']
+            else:
+                merged_champions[champion['name']] = Champion(champion['name'])
+                merged_champions[champion['name']].summary = champion['summary']
+                merged_champions[champion['name']].description = champion['description']
 
     summary = generate_summary(summaries)
-    
-    with codecs.open(os.path.join(out_dir, "summary"), "w", "utf-8") as file:
-        file.write(summary)
 
-    with codecs.open(os.path.join(out_dir, "champions"), "w", "utf-8") as file:
-        json.dump(champions, file, default=patch_serialize, indent=4)
+    for champion in merged_champions.values():
+        champion.summary = generate_sentence(champion.summary, 20, 1)
+        champion.description = generate_sentence(champion.description, 15, 2)
+
+    generated_patch = Patch('generated', summary)
+    generated_patch.champions = merged_champions
+
+    with codecs.open(os.path.join(out_dir, "patch"), "w", "utf-8") as file:
+        json.dump(generated_patch, file, default=patch_serialize, indent=4)
     
 
 if __name__ == "__main__":
